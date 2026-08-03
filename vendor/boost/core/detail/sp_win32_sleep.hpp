02b6f682153345e127c7f240152538abfe20a25b
Eduardo Rittner <eduardorittnerc@gmail.com>
2026-08-03 10:39:07 -0300

    fix windows CI

--- vendor/boost/core/detail/sp_win32_sleep.hpp
+++ vendor/boost/core/detail/sp_win32_sleep.hpp
@@ -9,9 +9,13 @@
 
 // boost/core/detail/sp_win32_sleep.hpp
 //
-// Provides the Win32 declarations for Sleep() and SwitchToThread()
-// used by sp_thread_sleep.hpp and sp_thread_yield.hpp when building
-// on Windows (including the MinGW-w64 cross-compiler).
+// Declares the Win32 Sleep() and SwitchToThread() functions used by
+// sp_thread_sleep.hpp and sp_thread_yield.hpp when building on Windows
+// (including the MinGW-w64 cross-compiler).
+//
+// The declarations are at global scope so that unqualified calls to
+// Sleep() / SwitchToThread() from within boost::core::detail resolve
+// correctly via global-namespace lookup.
 //
 // Copyright 2008, 2020 Peter Dimov
 // Distributed under the Boost Software License, Version 1.0
@@ -23,11 +27,6 @@
 # include <windows.h>
 #endif
 
-namespace boost
-{
-namespace detail
-{
-
 #if !defined( BOOST_USE_WINDOWS_H )
 
 extern "C" __declspec(dllimport) void __stdcall Sleep( unsigned long ms );
@@ -35,8 +34,5 @@ extern "C" __declspec(dllimport) void __stdcall Sleep( unsigned long ms );
 extern "C" __declspec(dllimport) int __stdcall SwitchToThread( void );
 
 #endif
-
-} // namespace detail
-} // namespace boost
 
 #endif // #ifndef BOOST_CORE_DETAIL_SP_WIN32_SLEEP_HPP_INCLUDED
